c1a7c3dbca5897373bed6a0cb5e0e9c93c87688a
devchux <chukwudieze97@gmail.com>
2022-07-13 18:43:48 +0100

    change url

--- src/components/navbar/TopNav.jsx
+++ src/components/navbar/TopNav.jsx
@@ -1,21 +1,27 @@
-import React from 'react'
-import { useTopNav } from '../../hooks/useTopNav';
-import "./scss/topNav.scss"
+import React from "react";
+import { useTopNav } from "../../hooks/useTopNav";
+import "./scss/topNav.scss";
 
 const TopNav = () => {
   const { toggleSideNav } = useTopNav();
   return (
     <div className="top-nav">
-      <div className="hamburger-wrapper" onClick={toggleSideNav} >
+      <div className="hamburger-wrapper" onClick={toggleSideNav}>
         <div></div>
         <div></div>
         <div></div>
       </div>
       <div className="link-wrapper">
-        <a href="javascript">Visit Site</a>
+        <a
+          href="https://lite-demo.vercel.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Visit Site
+        </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default TopNav
+export default TopNav;
